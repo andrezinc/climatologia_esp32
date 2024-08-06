4e8b6bd76832500ddd4d4281494f0fa2bc107308
Andre Castilhano <71910133+andrezinc@users.noreply.github.com>
2024-08-06 10:50:37 -0300

    Renomeando em portugues

--- projeto_climatologia.ino
+++ projeto_climatologia.ino
@@ -3,36 +3,86 @@
 #include <WiFi.h> //Biblioteca de wifi
 #define pino_dht 4
 #define tip_dht DHT11
-#define IO_USERNAME  "xxx"
-#define IO_KEY       "xxx"
+#define USUARIO  "Climatologia"
+#define SENHA       "XXXXXXX"
 DHT dht(pino_dht, tip_dht);
-const char* ssid = "TP-xx";
-const char* password = "xxx";
-const char* mqtt_server = "xxxx";
+const char* ssid_wifi = "XXXXX";
+const char* senha_wifi = "XXXXXXX";
+const char* mqtt_server = "52.54.163.195";
 unsigned long tempo = 0;
+int reboot = 0;
+int leitura = 0;
 String temp_str;
 String umid_str;
 char temp[50];
 char umid[50];
 WiFiClient Esp;
-PubSubClient client(Esp);
-void callback(char* topic, byte* payload, unsigned int length);
-void setup_wifi();
-void reconnect();
-void send_m(float h, float t);
+PubSubClient cliente(Esp);
+void conexao_mqtt() {
+    while (!cliente.connected()) {
+        Serial.print("Tentando conexão com mqtt");
+        String clienteId = "ESP32_";
+        clienteId += String(random(0xffff), HEX);
+
+        if (cliente.connect(clienteId.c_str(), USUARIO, SENHA, "Climatologia/feeds/esta-on", 1, true, "Desligado")) {
+            Serial.println("Conectado");
+            cliente.publish("Climatologia/feeds/esta-on", "Ligado", true);
+            cliente.subscribe("Climatologia/feeds/reboot");
+            cliente.subscribe("Climatologia/feeds/ativar-temp-umid");
+
+
+        } else {
+            Serial.print("falha, rc=");
+            Serial.print(cliente.state());
+            Serial.println(" tentando novamente em 5 segundos");
+            delay(5000);
+        }
+    }
+}
+
+void retorno_mensagem(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Mensagem do topico [");
+    Serial.print(topic);
+    Serial.print("]:");
+    if (strcmp(topic, "Climatologia/feeds/reboot") == 0) {
+        reboot = 1;
+    }
+    if (strcmp(topic, "Climatologia/feeds/ativar-temp-umid") == 0) {
+        leitura = 1;
+    }
+    Serial.println();
+}
+void configuracao_wifi() {
+    WiFi.begin(ssid_wifi, senha_wifi);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("conetando...");
+    }
+    Serial.println("conectou");
+
+}
+void send_m(float h, float t) {
+    umid_str = String((String)h );
+    temp_str = String((String)t );
+
+    umid_str.toCharArray(umid, umid_str.length() + 1);
+    temp_str.toCharArray(temp, temp_str.length() + 1);
+    cliente.publish("Climatologia/feeds/umid", umid);
+    cliente.publish("Climatologia/feeds/temp", temp);
+}
 void setup() {
   Serial.begin(9600);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  configuracao_wifi();
+  cliente.setServer(mqtt_server, 1883);
+  cliente.setCallback(retorno_mensagem);
   dht.begin();
 }
 void loop() {
-  if (!client.connected()) {
-    reconnect();
+  if (!cliente.connected()) {
+    conexao_mqtt();
   }
-  client.loop();
-  if (millis() - tempo > 900000) {
+  cliente.loop();
+  if (leitura) {
     float h = 0;
     float t = 0;
     for (int i = 0; i < 10000; i++) {
@@ -42,52 +92,12 @@ void loop() {
     t = t / 10000;
     h = h / 10000;
     send_m(h, t);
-    tempo = millis();
+    leitura = 0;
+  }
+  if (reboot == 1) {
+    ESP.restart();
   }
 }
 
-void setup_wifi() {
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("conetando...");
-  }
-  Serial.println("conectou");
 
-}
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Mensagem do topico [");
-  Serial.print(topic);
-  Serial.print("]:");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Tentando conexão com mqtt");
-    String clientId = "ESP32_Andre-";
-    clientId += String(random(0xffff), HEX);
 
-    if (client.connect(clientId.c_str(), IO_USERNAME, IO_KEY, "Climatologia/feeds/esta-on", 1, true, "Desligado")) {
-      Serial.println("Conectado");
-      client.publish("Climatologia/feeds/esta-on", "Ligado", true);
-      client.subscribe("Climatologia/feeds/teste");
-    } else {
-      Serial.print("falha, rc=");
-      Serial.print(client.state());
-      Serial.println(" tentando novamente em 5 segundos");
-      delay(5000);
-    }
-  }
-}
-void send_m(float h, float t) {
-  umid_str = String((String)h );
-  temp_str = String((String)t );
-
-  umid_str.toCharArray(umid, umid_str.length() + 1);
-  temp_str.toCharArray(temp, temp_str.length() + 1);
-  client.publish("Climatologia/feeds/umid-andre", umid);
-  client.publish("Climatologia/feeds/temp-andre", temp);
-}
